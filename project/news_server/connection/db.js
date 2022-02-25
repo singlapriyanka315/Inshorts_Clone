300e39fd863491d36c70404fb1610513aa85be22
Priyanka Singla <priyanka0363.cse19@chitkara.edu.in>
2022-02-26 01:18:43 +0530

    Update db.js

--- project/news_server/connection/db.js
+++ project/news_server/connection/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 
 const Connection = async () => {
     try {   
-        const URL = `mongodb://priya:priya@cluster0-shard-00-00.ibzko.mongodb.net:27017,cluster0-shard-00-01.ibzko.mongodb.net:27017,cluster0-shard-00-02.ibzko.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-r5x4lq-shard-0&authSource=admin&retryWrites=true&w=majority`
+        const URL = `mongodb://<username>:<password>@cluster0-shard-00-00.ibzko.mongodb.net:27017,cluster0-shard-00-01.ibzko.mongodb.net:27017,cluster0-shard-00-02.ibzko.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-r5x4lq-shard-0&authSource=admin&retryWrites=true&w=majority`
 
    await mongoose.connect(URL,{useNewUrlParser: true})
     
